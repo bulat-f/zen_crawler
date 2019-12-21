743e5f52efd9a05c93ba9fa622f5221ae99401ff
Bulat F <write2bulat@gmail.com>
2019-12-21 18:35:29 +0300

    Correct typo in code

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 This project created for fetching data from [yandex.zen](https://zen.yandex.ru) channel. For now from one specific (although it can be changed easealy by changing a const in `main.rb`)
 
+## How to run
+
+To run the script use:
+`ruby main.rb`
+
+Parsed data will be saved in `results.json`.
+
 ## Todo
 
 1. Implement it as ruby gem
 2. Write tests
+3. Format `results.json` to get pretty data
--- main.rb
+++ main.rb
@@ -32,20 +32,6 @@ output.close
 ratio_of_tt = tt_count.to_f / parsed_data.length * 100
 
 puts
+puts
 puts "#{parsed_data.length} articles are parsed"
 puts "#{tt_count} (#{ratio_of_tt.round}%) of them are tatar"
-puts
-
-max_views = parsed_data.max { |a, b| a[:views] <=> b[:views] }
-max_views_till_end = parsed_data.max do |a, b|
-  a[:max_views_till_end] <=> b[:max_views_till_end]
-end
-most_readable = parsed_data.max do |a, b|
-  a_ratio = a[:max_views_till_end].to_f / a[:views]
-  b_ratio b[:max_views_till_end].to_f / b[:views]
-  a_ratio <=> b_ratio
-end
-
-puts "Most viewed publication is #{max_views[:title]}"
-puts "Most viewed till the end publication is #{max_views_till_end[:title]}"
-puts "Most readable publication is #{most_readable[:title]}"
